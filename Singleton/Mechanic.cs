411d4f8f833ea93262435612dc5280bb83d659e1
Whysoezzy <n.mamonovv@gmail.com>
2023-12-04 15:23:36 +0300

    init

---
+++ Singleton/Mechanic.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton;
+
+public class Mechanic
+{
+    private static Mechanic _instance = new ();
+
+    protected Mechanic()
+    {
+     
+    }
+
+    public static Mechanic GetMechanic()
+    {
+        if (_instance == null)
+        {
+            _instance = new Mechanic();
+        }
+
+        return _instance;
+    }
+    public void CheckProblem(string problem)
+    {
+        Console.WriteLine("The mechanic examined the " + problem);
+    }
+    public void Repair(string detail)
+    {
+        Console.WriteLine("The mechanic started repairing the " + detail);
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Singleton;
+
+Mechanic m1 = Mechanic.GetMechanic();
+Mechanic m2 = Mechanic.GetMechanic();
+Mechanic m3 = Mechanic.GetMechanic();
+
+if (m1 == m2 && m2 == m3)
+{
+    Console.WriteLine("Same instance\n");
+}
+m1.CheckProblem("dent on the door");
+m1.Repair("door");
+
+m2.CheckProblem("dent on the hood");
+m2.Repair("hood");
+
+m3.CheckProblem("jammed threshold");
+m3.Repair("threshold");
